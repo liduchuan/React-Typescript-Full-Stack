f8b24da36831d674953b3f414582089edc0d6402
liduchuan <alex.liduchuan@gmail.com>
2025-09-02 20:29:17 +0800

    feat: add build script and update dependencies in package.json; enhance App component with Button

--- packages/client/src/renderer/src/App.tsx
+++ packages/client/src/renderer/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from 'ui'
+
 function App(): React.JSX.Element {
-  return <div className="text-2xl font-bold underline">hello world</div>
+  return (
+    <div className="text-2xl font-bold underline">
+      hello world!
+      <Button />
+    </div>
+  )
 }
 
 export default App
